b13e3348c27dd2aa0c9bea3e4b346ed24bec6554
Tony Bowden <tony@tmtm.com>
2017-04-11 07:58:53 +0100

    Update for changed Wikipedia heading

--- scraper.rb
+++ scraper.rb
@@ -12,7 +12,7 @@ fr_names = EveryPolitician::Wikidata.wikipedia_xpath(
 ar_col = 'الحزب'
 ar_names = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: "https://ar.wikipedia.org/wiki/قائمة_أعضاء_مجلس_نواب_الشعب_التونسي_الأول",
-  after: '//span[@id=".D8.A7.D9.84.D8.A3.D8.B9.D8.B6.D8.A7.D8.A1_.D8.A7.D9.84.D8.AF.D8.A7.D8.A6.D8.B1.D8.A9_.D8.A7.D9.84.D8.A7.D9.86.D8.AA.D8.AE.D8.A7.D8.A8.D9.8A.D8.A9"]',
+  after: '//span[@id=".D8.A7.D9.84.D8.A3.D8.B9.D8.B6.D8.A7.D8.A1_.D8.AD.D8.B3.D8.A8_.D8.A7.D9.84.D8.AF.D8.A7.D8.A6.D8.B1.D8.A9_.D8.A7.D9.84.D8.A7.D9.86.D8.AA.D8.AE.D8.A7.D8.A8.D9.8A.D8.A9"]',
   xpath: '//table[.//th[.="%s"]]//td[3]//a[not(@class="new")]/@title' % ar_col,
 ) 
 
